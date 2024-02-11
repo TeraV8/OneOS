a55ee6553b5d3d64fbdc4678a86357e0a0e729e2
TeraV <vharris1800@gmail.com>
2024-02-10 21:00:30 -0600

    printStr very important!

--- src/boot.asm
+++ src/boot.asm
@@ -1,7 +1,26 @@
 [org 0x7c00]
 
+mov si, STR_TEST_MESSAGE
+call printStr
+
 jmp $
 
-times 510 - ($ - $$) db 0
+printStr:
+    pusha
+    _printStr_loop:
+        mov al, [si]
+        cmp al, 0
+        jne _printStr_char
+        popa
+        ret
+    _printStr_char:
+        mov ah, 0x0e
+        int 0x10
+        add si, 1
+        jmp _printStr_loop
 
+STR_TEST_MESSAGE: db "This is a message that is testing. messing testage.", 0x0a, 0x0d, 0
+
+; IMPORTANT this is end of bootsector
+times 510 - ($ - $$) db 0
 dw 0xaa55
