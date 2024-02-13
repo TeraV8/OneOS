daf1bd875aef6743243fb9c69280ed9a0c7828fb
TeraV <vharris1800@gmail.com>
2024-02-12 21:39:20 -0600

    Changed from null-terminated strings to indexed ones Centered error message text

--- src/boot-core.asm
+++ src/boot-core.asm
@@ -1,19 +1,19 @@
-; Prints null-terminated string to the display (teletype output)
-; si - String pointer
 printStr:
     pusha
+    mov ah, 0x0e
+    xor bh, bh
+    mov cl, [si]
     _printStr_loop:
+        cmp cl, 0
+        je _printStr_end
+        dec cl
+        inc si
         mov al, [si]
-        cmp al, 0
-        jne _printStr_char
+        int 0x10
+        jmp _printStr_loop
+    _printStr_end:
         popa
         ret
-    _printStr_char:
-        mov ah, 0x0e
-        xor bh, bh
-        int 0x10
-        add si, 1
-        jmp _printStr_loop
 
 ; Clears the screen of all text
 ; bh - text attributes
@@ -31,7 +31,7 @@ clearScreen:
     ret
 
 ; Clears the screen to display error message (never returns)
-; si - String pointer
+; si - String pointer (indexed)
 displayError:
     push si
     mov bh, 0x4f
@@ -43,15 +43,24 @@ displayError:
     mov si, STR_ERROR_GENERIC
     call printStr
     mov ah, 0x02
-    mov dx, 0x0c00
-    int 0x10
+    mov dh, 0x0c
     pop si
+    ; All this is necessary to center the message
+    mov cl, [si]
+    mov dl, 80
+    sub dl, cl
+    shr dl, 1
+    jnc _displayError_skipRightBump
+    inc dl
+    _displayError_skipRightBump:
+    ;;
+    int 0x10
     call printStr
     mov ah, 0x01
     mov cx, 0x2706
     int 0x10
-    mov ax, 0x0e07
-    xor bh, bh
+    ;mov ax, 0x0e07      ; Not sure why this was here?
+    ;xor bh, bh          ; ditto
     jmp $
 
-STR_ERROR_GENERIC: db "OneOS encountered an error during the boot process.", 0
+STR_ERROR_GENERIC: db 51, "OneOS encountered an error during the boot process."
