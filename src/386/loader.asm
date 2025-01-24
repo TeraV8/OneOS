188ac667312212119ac7fe289db8f6d2b7870f99
TeraV8 <89698857+TeraV8@users.noreply.github.com>
2025-01-24 17:20:58 -0500

    printHex functions and debug info

--- src/386/loader.asm
+++ src/386/loader.asm
@@ -8,6 +8,20 @@ resw 1
 call println
 mov si, STR_STAT_LOADER_OK
 call logOutput
+mov si, STR_TEMP_LOADER
+call printStr
+mov si, STR_INFO_BOOTDISK
+call printStr
+mov al, [disk_id]
+call printHex8
+call println
+mov si, STR_TEMP_LOADER
+call printStr
+mov si, STR_INFO_EOTLABEL
+call printStr
+mov ax, EOT_ADDR
+call printHex16
+call println
 
 jmp $
 
@@ -47,9 +61,55 @@ println:
     pop ax
     ret
 
+printHex8:
+    push ax
+    push bx
+    push ax
+    mov ah, 0x0e
+    movzx bx, al
+    shr bl, 4
+    mov al, [HEXTABLE + bx]
+    mov bx, 0x0007
+    int 0x10
+    pop ax
+    mov ah, 0x0e
+    movzx bx, al
+    and bl, 0b00001111
+    mov al, [HEXTABLE + bx]
+    mov bx, 0x0007
+    int 0x10
+    pop bx
+    pop ax
+    ret
+
+printHex16:
+    push ax
+    push bx
+    push ax
+    movzx bx, ah
+    mov ah, 0x0e
+    shr bl, 4
+    mov al, [HEXTABLE + bx]
+    mov bx, 0x0007
+    int 0x10
+    pop ax
+    movzx bx, ah
+    mov ah, 0x0e
+    and bl, 0b00001111
+    mov al, [HEXTABLE + bx]
+    mov bx, 0x0007
+    int 0x10
+    pop bx
+    pop ax
+    jmp printHex8           ; tail call
+
 STR_TEMP_LOADER: db "loader: ", 0
 STR_STAT_LOADER_OK: db "Loader successfully initialized!", 0
+STR_INFO_BOOTDISK: db "Boot disk: 0x", 0
+STR_INFO_EOTLABEL: db "End of text address: 0x", 0
+HEXTABLE: db "0123456789abcdef"
 
+EOT_ADDR:
 times (32256 - ($ - $$)) db 0
 
 %include "shared.asm"
